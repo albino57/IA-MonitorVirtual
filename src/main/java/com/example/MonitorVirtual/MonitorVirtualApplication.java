497a53d6d9b3935a4886c60940f996d919925232
Rafael Albino <ansdles@live.com>
2026-06-27 15:36:00 -0300

    feat: subindo base do projeto

---
+++ src/main/java/com/example/MonitorVirtual/MonitorVirtualApplication.java
@@ -0,0 +1,13 @@
+package com.example.MonitorVirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorVirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MonitorVirtual/MonitorVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MonitorVirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
